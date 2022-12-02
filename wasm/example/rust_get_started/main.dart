3cf803d53c6d4913e05ded922aa1772b2534c68a
Modestas Valauskas <valauskasmodestas@gmail.com>
2022-12-02 08:36:18 +0100

    Start

---
+++ wasm/example/rust_get_started/main.dart
@@ -0,0 +1,7 @@
+/// This example is based on https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm
+void main() {
+  // TODO load the wasm file.
+  // TODO output info.
+  // TODO call hello thing.
+  // TODO print output.
+}
